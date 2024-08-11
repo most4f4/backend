822d09a95b4ff976dba5d39a01ce25ded875768b
Mostafa Hasanalipourshahrabadi <mostafa.shahrabadi69@yahoo.com>
2024-08-10 21:59:05 -0400

    after config change

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = function() {
-    mongoose.connect(process.env.MONGODB_URI)
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
     .catch(err => {
         console.error(err.message);
